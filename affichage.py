bb3c0fe6987fbbbac47401682a3c1f2abdf4774c
loubaut <thibaulttampigny@gmail.com>
2023-12-21 19:57:24 +0100

    presque

--- affichage.py
+++
@@ -1,8 +0,0 @@
-def fonction_affichage(resultat, somme_a_rendre):
-    """
-    Doc-String:
-    Entrée: 
-    Sortie: 
-    """
-    for key, value in resultat.items():
-        print(f"Voilà {value} billets de {key} pour une somme totale de {somme_a_rendre}:")
